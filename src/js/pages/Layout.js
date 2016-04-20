df5567b03a3923bfc35bf5faf0ed1c81787dd512
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2016-04-20 22:18:21 +0100

    refactoring data loading

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,15 +1,52 @@
 import React from "react";
-import LoadData from "../components/loadData";
+
 
 require('../../styles/main.scss');
 
 
 let LayoutPage = React.createClass({
 
+	getInitialState: function(){
+		return {
+			response: "waiting"
+		}
+	},
+	init: function() {
+		let self = this;
+		this.loadJSON('./data/looca.json', function(results) {
+			window.localStorage.setItem("appData", JSON.stringify(results));
+			self.setState({'response':'ready'});
+		});
+	},
+	componentWillMount: function(){
+		this.init();
+	},
+	loadJSON(path, success, error){
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function(){
+			if (xhr.readyState === XMLHttpRequest.DONE) {
+				if (xhr.status === 200) {
+					if (success) {
+						success(JSON.parse(xhr.responseText));
+					}
+				} else {
+					if(error) {
+						error(xhr);
+					}
+				}
+			}
+		};
+		xhr.open("GET", path, true);
+		xhr.send();
+	},
 	render() {
+		let PageContent = "Loading...";
+		if(this.state.response == "ready"){
+			PageContent =	this.props.children;
+		}
 		return (
 			<div>
-				{this.props.children}
+				{PageContent}
 			</div>
 		);
 	}
