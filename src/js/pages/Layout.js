7ff5378b9088a68aef2393f78aee897465179df5
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2016-04-24 22:52:09 +0100

    add expire date to localstorage

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,25 +1,38 @@
 import React from "react";
-
-
 require('../../styles/main.scss');
 
-
 let LayoutPage = React.createClass({
-
-	getInitialState: function(){
+	getInitialState(){
 		return {
 			response: "waiting"
 		}
 	},
-	init: function() {
+	init(){
+		const days = 30; // Reset when storage is more than 31 days
+		const timeNow = new Date().getTime();
+		const setupTime = localStorage.getItem('loocaSetupTime');
+		if (setupTime == null) {
+			localStorage.setItem('loocaSetupTime', timeNow);
+			this.setData();
+		} else {
+			if(timeNow - setupTime > days*24*60*60*1000) {
+				localStorage.removeItem('loocaSetupTime');
+				localStorage.setItem('loocaSetupTime', timeNow);
+				this.setData();
+			} else {
+				this.setState({'response':'ready'});
+			}
+		}
+	},
+	componentWillMount(){
+		this.init();
+	},
+	setData(){
 		let self = this;
-		this.loadJSON('./data/looca.json', function(results) {
+		self.loadJSON('./data/looca.json', function(results) {
 			window.localStorage.setItem("appData", JSON.stringify(results));
 			self.setState({'response':'ready'});
 		});
-	},
-	componentWillMount: function(){
-		this.init();
 	},
 	loadJSON(path, success, error){
 		let xhr = new XMLHttpRequest();
